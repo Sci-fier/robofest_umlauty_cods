0d81150c2d295caa2bcaf0f828a6899dd9cfa85d
Sci-fier <119115673+Sci-fier@users.noreply.github.com>
2024-01-25 16:04:48 +0300

    Update Accept.ino

--- Accept.ino
+++ Accept.ino
@@ -16,18 +16,35 @@
 #define PIN_IN1L 17 // INPUT 1 LEFT
 #define PIN_IN2L 16 // INPUT 2 LEFT
 #define PIN_ENR 5 // ENABLE RIGHT
-#define PIN_IN1R 14 // INPUT 1 RIGHT
+#define PIN_IN1R 3 // INPUT 1 RIGHT
 #define PIN_IN2R 15 // INPUT 2 RIGHT
-#define PIN_RELAY 3 // RELAY OF RESERVE POWER
+#define PIN_POT 14 // PIN POT
 
 //первичная настройка радиомодуля
-RF24 radio(7, 8); // порты D9, D10: CE (SS - выбор ведомого)
+RF24 radio(7, 8); // порты D7, D8: CE (SS - выбор ведомого)
 const uint32_t pipe = 111156789; // адрес рабочей трубы
 long int data[7]; // массив передачи
-ServoSmooth servox; // создание переменной, отвечающей за плавное движение по х класса ServoSmooth
-ServoSmooth servoy; // создание переменной, отвечающей за плавное движение по у класса ServoSmooth
+Servo servox; // создание переменной, отвечающей за плавное движение по х класса ServoSmooth
+Servo servoy; // создание переменной, отвечающей за плавное движение по у класса ServoSmooth
 Multiservo servo_arm; // создание переменной, отвечающей за изменение угла наклона манипулятора, класса Multiservo
 Multiservo servo_claw; // создание переменной, отвечающей за управление клешнёй манипулятора, класса Multiservo
+
+void arm_move(int pos){
+  pos = map(pos, 0, 1023, 500, 200);
+  Serial.print(pos);
+  Serial.print(' ');
+  Serial.print(analogRead(PIN_POT));
+  Serial.print(' ');
+  int q = pos - analogRead(PIN_POT);
+  Serial.print(q);
+  Serial.print(' ');
+  q = map(q, 120, -120, 0, 180);
+  if(q<0){q=0;}
+  if(q>180){q=180;} 
+  Serial.print(q);
+  Serial.print(' ');
+  servo_arm.write(q);
+}
 
 // создание функции движения
 void robot_move(int SL, int SR){
@@ -74,9 +91,7 @@ void setup() {
   servoy.attach(PIN_SY); // резервирование порта PIN_SY под servoy
   // моторы для манипулятора принимают другую длину импульса (1000 - минимальная длина импульса, 2000 - максимальная длина импульса)
   servo_arm.attach(PIN_SA, 1000, 2000); // резервирование порта PIN_SA под servo_arm 
-  servo_claw.attach(PIN_SC, 1000, 2000); // резервирование порта PIN_SA под servo_claw
-  servox.setAccel(1.0); //задаём максимальное ускорение для сервопривода оси Х
-  servoy.setAccel(1.0); //задаём максимальное ускорение для сервопривода оси Y
+  servo_claw.attach(PIN_SC, 1000, 2000); // резервирование порта PIN_SA под servo_cla
 }
 
 void loop() {
@@ -90,29 +105,24 @@ void loop() {
     // 4 - скорость врашения мотора в клешне от 0 до 2
     // 5 - скорость врашения мотора в подьемном механизме от 0 до 2
     // 6 - показывает необходимость включения резервного питания от 0 до 1
-    servox.setTargetDeg(data[0]); // задаем целевой угол (угол в градусах) для оси Y
-    servox.tick(); // начало движения камеры по оси X
-    servoy.setTargetDeg(data[1]); // задаем целевой угол (угол в градусах) для оси Х
-    servoy.tick(); // начало движения камеры по оси Y
+    servox.write(data[0]); // задаем целевой угол (угол в градусах) для оси Y
+    servoy.write(data[1]); // задаем целевой угол (угол в градусах) для оси Х
     robot_move(data[2], data[3]); // движение робота на координаты data[2], data[3]
     servo_claw.write(data[4] * 90); // задаём вращение клешни
-    servo_arm.write(data[5] * 90); // задаём вращение манипулятора
-    // проверяем сигнал на включение резервного питания
-    if(data[6] == 1){
-      analogWrite(PIN_RELAY, 0); // включаем резервное питание (перемещаем реле в позицию 0)
-    }
-    else{
-      analogWrite(PIN_RELAY, 255); // выключаем резервное питание (перемещаем реле в позицию 255)
-    }
+    arm_move(data[5]); // задаём положение манипулятора
 #ifdef DEBUG
     //проверка выходных значений
-    for(int i=0; i < 7; ++i){
+    for(int i=0; i < 6; ++i){
       Serial.print(data[i]);
       Serial.print(" ");
     }
     Serial.println("");
   }
+  else{
+    robot_move(255, 255);
+    servo_claw.write(90); 
+  }
 #endif 
 
-  delay(40);
+  delay(25);
 }
